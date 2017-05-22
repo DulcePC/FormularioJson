b643ed6a42fbc88e1f7f35484756fef11d84010a
dulcepc <dulcefrancelispc@gmail.com>
2017-05-21 23:34:54 -0400

    los datos son guardados en las tablas

--- js.js
+++ js.js
@@ -6,8 +6,57 @@ function save(){
 	for(i=0; i < campos.length; i++){
 		cmp = campos[i];
 		fila[cmp.id] = cmp.value;
+		cmp.value=' ';
 	}
 	console.log(fila);
 	DATOS.push(fila);
-	localStorage.setItem('DATOS', JSON.stringify(DATOS));	
+	localStorage.setItem('DATOS', JSON.stringify(DATOS));
+	mostrarDatos();
+}
+
+window.onload= function(){
+	tmp= localStorage.getItem('DATOS');
+	if(tmp != null){
+		DATOS=JSON.parse(tmp);
+		mostrarDatos();	
+	}
+}
+function _dce(idd,extra){
+	tmp=document.createElement(idd);
+	if(extra){
+		for(k in extra){
+			tmp.setAttribute(k, extra[k]);
+		}
+	}
+	return tmp;
+}
+function mostrarDatos(){
+	tbl=_dce('table');
+	thead=_dce('thead');
+	tbody=_dce('tbody');
+	tbl.appendChild(thead);
+	tbl.appendChild(tbody);
+	tr=_dce('tr');
+	if(DATOS[0]){
+		fila=DATOS[0];
+		for(k in fila){
+			th = _dce('th');
+			th.innerHTML=k;
+			tr.appendChild(th);
+		}
+		thead.appendChild(tr);
+		for(k in DATOS){
+			fila=DATOS[k];
+			tr=_dce('tr');
+			for(y in fila){
+				th=_dce('td');
+				th.innerHTML=fila[y];
+				tr.appendChild(th);
+			}
+			tbody.appendChild(tr);
+		}
+
+	}
+	document.getElementById('listaDatos').innerHTML = tbl.innerHTML;
+
 }
